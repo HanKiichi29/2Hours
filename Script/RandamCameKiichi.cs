f8e7f0f21b3df58091b81051775c0e85f3c48ddb
HanKiichi29 <92429317+HanKiichi29@users.noreply.github.com>
2021-12-01 19:08:15 +0900

    スクリプトを追加
    現状ゲームをするとテキストが表示されていない状態だったので再度ビルドして、ゲームデータをアップロードします

---
+++ Script/RandamCameKiichi.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UniRx;
+
+public class RandamCameKiichi : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Kiichi;
+    [SerializeField] private float KiichiSpeed;
+    [SerializeField] private Button PikUpButton;
+    [SerializeField] private AudioClip Tyarin;
+    [SerializeField] private ScoreScript _scoreScript;
+
+
+    private Vector2 D_transform;
+    private int RandomNum;
+
+    private AudioSource audi;
+
+    private void Start()
+    {
+        PikUpButton.GetComponent<Button>();
+        audi = GetComponent<AudioSource>();
+        D_transform = Kiichi.transform.position;
+
+        
+    }
+
+
+    public void RandomCome()
+    {
+        RandomNum = Random.Range(0, 10);
+        audi.PlayOneShot(Tyarin);
+
+        if (RandomNum == 0)
+        {
+
+            _scoreScript.ScoreCheck();
+            PikUpButton.interactable = false;
+            audi.PlayOneShot(audi.clip);
+            Kiichi.transform.DOMoveX(-20, KiichiSpeed)
+                .SetEase(Ease.Linear)
+                .SetLink(gameObject)
+                .OnComplete(() => { Invoke("Refresh", 1.3f);  });
+
+            Initialize();
+
+        }
+        else
+        {
+            _scoreScript.AddScore();
+        }
+
+        
+
+
+    }
+
+    private void Initialize()
+    {
+        _scoreScript.ScoreReset();
+    }
+
+    private void Refresh()
+    {
+        Kiichi.transform.position = D_transform;
+        PikUpButton.interactable = true;
+    }
+}
---
+++ Script/ScoreScript.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    [SerializeField] private Text ScoreText;
+    [SerializeField] private Text HightScoreText;
+    [SerializeField] private Text S_HightScore;
+    [SerializeField] private Text TotyuuScore;
+
+
+    [SerializeField] private List<AudioClip> ToTyuu = new List<AudioClip>();
+   
+
+
+    private int ScoreNum;
+    private int HightScoreNum;
+
+
+    private AudioSource audioSource;
+    
+
+
+    private void Start()
+    {
+        audioSource=GetComponent<AudioSource>();
+        HightScoreText.text = "ハイスコア：" + HightScoreNum.ToString();
+        ScoreReset();
+    }
+
+
+    public void ScoreReset()
+    {
+        ScoreNum = 0;
+        DrowText();
+    }
+
+    public void AddScore()
+    {
+
+        ScoreNum += 100;
+        TotyuuMoney();
+        DrowText();
+    }
+
+    private void DrowText()
+    {
+        ScoreText.text = "拾ったお金："+ScoreNum.ToString();
+
+    }
+
+    public void ScoreCheck()
+    {
+        if (HightScoreNum<ScoreNum)
+        {
+            
+            HightScoreNum = ScoreNum;
+            HightScoreText.text = "ハイスコア："+HightScoreNum.ToString();
+            Invoke("SayHight", 1);
+          
+
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void SayHight()
+    {
+        S_HightScore.DOFade(1, 1)
+            .SetLink(gameObject)
+            .SetLoops(2, LoopType.Yoyo);
+
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+   
+
+    private void TotyuuMoney()
+    {
+        if (ScoreNum == 1000)
+        {
+            audioSource.PlayOneShot(ToTyuu[0]);
+            fadeText();
+        }
+        else if (ScoreNum == 2000)
+        {
+            audioSource.PlayOneShot(ToTyuu[1]);
+            fadeText();
+        }
+        else if (ScoreNum == 3000)
+        {
+            audioSource.PlayOneShot(ToTyuu[2]);
+
+            fadeText();
+        }
+        else if (ScoreNum == 4000)
+        {
+            audioSource.PlayOneShot(ToTyuu[3]);
+            fadeText();
+        }
+        else if (ScoreNum == 5000)
+        {
+            audioSource.PlayOneShot(ToTyuu[4]);
+            fadeText();
+        }
+    }
+
+
+    private void fadeText()
+    {
+        TotyuuScore.text = ScoreNum.ToString() + "円突破！！";
+
+        TotyuuScore.DOFade(1, 0.5f)
+            .SetLink(gameObject)
+            .SetEase(Ease.Linear)
+            .SetLoops(2, LoopType.Yoyo);
+    }
+}
